618a176841e04ba5a7a022ba5c219b83e4552ef2
Yuvraj Kumar <yuvrajkumar@Yuvrajs-MacBook-Air.local>
2026-02-16 16:35:25 +0530

    changed admin login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch("${API_URL}/api/auth/login", {
+      const response = await fetch(`${API_URL}/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
